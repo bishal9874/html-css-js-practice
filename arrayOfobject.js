81effa6b9af6e571a1160482179568a7199e3dda
bishal9874 <bm8130377@gmail.com>
2024-03-29 23:42:51 +0530

    new

---
+++ arrayOfobject.js
@@ -0,0 +1,19 @@
+let student = [
+    {
+        name : "bishal",
+        age: 25
+    },
+    {
+        name : "dasda",
+        age: 28
+    },
+    {
+        name : "bisfghhgfhal",
+        age: 22
+    }
+]
+
+student.forEach((ele)=>{
+    console.log(ele.name);
+    console.log(ele.age);
+})
